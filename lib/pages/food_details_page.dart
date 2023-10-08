9875906446cc16c33638729940c571997e85c741
mgagliardi.dev <mgagliardi.dev@gmail.com>
2023-10-08 18:27:48 +0200

    layout fixes food_details

--- lib/pages/food_details_page.dart
+++ lib/pages/food_details_page.dart
@@ -78,9 +78,9 @@ class _FoodDetailsState extends State<FoodDetails> {
                   )
                 ],
               ),
-
+      
             const SizedBox(height: 20),
-
+      
             const Text(
               'Ingredients: ',
               style: TextStyle( 
@@ -89,93 +89,94 @@ class _FoodDetailsState extends State<FoodDetails> {
                   fontWeight: FontWeight.w500
                 ),
             ),
-
+      
             const SizedBox(height: 20),
-
-            const Text(
-              'TODO: Implement food ingeredients at db',
-              style: TextStyle( 
-                  fontSize: 20,
-                  fontFamily: 'Roboto',
-                  color: Colors.black38
-                ),
+      
+            const Expanded(
+              child: Text(
+                'TODO: Implement food ingeredients at db',
+                style: TextStyle( 
+                    fontSize: 20,
+                    fontFamily: 'Roboto',
+                    color: Colors.black38
+                  ),
+              ),
             ),
-            
-            const SizedBox(height: 180),
-
+      
             // button row
             Align(
-              alignment: Alignment.centerLeft,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 128, 53, 53),
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.all(Radius.circular(150))
-                    ),
-                    height: 70,
-                    width: 260,
-                    child: Row (
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-
-                        const SizedBox(width: 10),
-                        
-                        // price
-                        Text(
-                          '$totalPrice\$',
-                          style: const TextStyle(
-                            fontSize: 23,
-                            color: Colors.white
-                          ),
-                        ),
-
-                        //SizedBox(width: 50),
-
-                        // minus btn
-                        AddSubBtn(symbol: '-', onPressed: () => updateFoodCounterAndPrice(-1)),
-
-                        // object counter
-                        Container(
-                          width: 30,
-                          alignment: Alignment.center,
-                          child: Text(
-                            foodCounter.toString(),
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 35),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 128, 53, 53),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.all(Radius.circular(150))
+                      ),
+                      height: 70,
+                      width: 260,
+                      child: Row (
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                    
+                          const SizedBox(width: 10),
+                          
+                          // price
+                          Text(
+                            '$totalPrice\$',
                             style: const TextStyle(
                               fontSize: 23,
                               color: Colors.white
                             ),
                           ),
-                        ),
-
-                        // plus btn
-                        AddSubBtn(symbol: '+', onPressed: () => updateFoodCounterAndPrice(1)),
-                      ],
-                    ),
-                  ),
-
-                  //const SizedBox(width: 15),
-
-                  ElevatedButton(
-                    onPressed: (foodCounter > 0) ? (){} : null,
-                      style: ElevatedButton.styleFrom(
-                      shape: const CircleBorder(),
-                      padding: const EdgeInsets.all(12),
-                      backgroundColor: const Color.fromARGB(255, 128, 53, 53), // <-- Button color
-                      foregroundColor: Colors.red, // <-- Splash color
-                      disabledBackgroundColor: const Color.fromARGB(150, 128, 53, 53)
-                    ), 
-                    child: 
-                      const Icon(
-                        Icons.shopping_cart_checkout_rounded,
-                        color: Colors.white,
-                        size: 38,
+                                
+                          // minus btn
+                          AddSubBtn(symbol: '-', onPressed: () => updateFoodCounterAndPrice(-1)),
+                    
+                          // object counter
+                          Container(
+                            width: 30,
+                            alignment: Alignment.center,
+                            child: Text(
+                              foodCounter.toString(),
+                              style: const TextStyle(
+                                fontSize: 23,
+                                color: Colors.white
+                              ),
+                            ),
+                          ),
+                    
+                          // plus btn
+                          AddSubBtn(symbol: '+', onPressed: () => updateFoodCounterAndPrice(1)),
+                        ],
                       ),
-                  )
-                ],
+                    ),
+                    
+                    // add to cart btn
+                    ElevatedButton(
+                      onPressed: (foodCounter > 0) ? (){} : null,
+                        style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(12),
+                        backgroundColor: const Color.fromARGB(255, 128, 53, 53), // <-- Button color
+                        foregroundColor: Colors.red, // <-- Splash color
+                        disabledBackgroundColor: const Color.fromARGB(150, 128, 53, 53) // <-- color when onPressed is null
+                      ), 
+                      child: 
+                        const Icon(
+                          Icons.shopping_cart_checkout_rounded,
+                          color: Colors.white,
+                          size: 38,
+                        ),
+                    )
+                  ],
+                ),
               ),
             )
         ]),
